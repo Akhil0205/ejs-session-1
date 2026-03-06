deff20a459aa3723c9d8b59644e5ddc5e1f1dc80
Akhil Mohan <akhil205.mohan@gmail.com>
2026-03-06 19:22:11 +0530

    update

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import session from 'express-session'
+const app=express()
+app.set("view engine","ejs")
+app.set("views","views")
+app.use(express.urlencoded({extended:true}))
+const users=[
+    {id:1,name:"John",email:"j@gmail.com",password:"1234"},
+    {id:2,name:"Cathy",email:"c@gmail.com",password:"1234"},
+    {id:1,name:"John",email:"j@gmail.com",password:"1234"},
+
+]
+app.use(session({
+    secret:"secretkey",
+    resave:false,
+    saveUninitialized:false,
+})
+)
+app.get("/",(req,res)=>
+    {
+        res.render("dashboard");
+
+    })
+app.get("/login",(req,res)=>
+    {
+        res.render("login");
+        
+    })    
+app.listen(8080,()=>
+    {
+        console.log("Server started")
+    })
